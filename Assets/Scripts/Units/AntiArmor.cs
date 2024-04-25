770222684b9e9564f75736799f38e56be98e1ce4
Noha SEVESTRE <noha.sevestre@supinfo.com>
2024-04-25 13:34:09 +0200

    Modif Script MOOVEMENT & DEGATS
    Regroupement des scripts

--- Assets/Scripts/Units/AntiArmor.cs
+++ Assets/Scripts/Units/AntiArmor.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class preMeleeMovement : MonoBehaviour
+public class AntiArmor : MonoBehaviour
 {
-    public Rigidbody2D preMeleeRb;
+    public Rigidbody2D AntiArmorRb;
     public float speed;
 
     // Update is called 50x per second
     void FixedUpdate()
     {
-        preMeleeRb.velocity = Vector2.right * speed;
+        AntiArmorRb.velocity = Vector2.right * speed;
     }
 }
---
+++ Assets/Scripts/Units/Melee.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour
+{
+    public int damage = 25;
+    public Rigidbody2D MeleeRb;
+    public float speed;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        HealthBar healthBar = collision.gameObject.GetComponent<HealthBar>();
+        if (healthBar != null)
+        {
+            healthBar.UpdateHealth(damage);
+        }
+    }
+
+    // Update is called 50x per second
+    void FixedUpdate()
+    {
+        MeleeRb.velocity = Vector2.right * speed;
+    }
+}
