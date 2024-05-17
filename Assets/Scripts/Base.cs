bc9ce98bf8c2f78577397979f285dfd0deeb1e33
Noha SEVESTRE <noha.sevestre@supinfo.com>
2024-05-17 16:54:39 +0200

    GameOver + MainMenu
    Panel Game Over
    3 Boutons
    Script
    Bouton MainMenu modifié (écriture)

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -6,4 +6,25 @@ public class Base : MonoBehaviour, IDamageable
 {
     public int maxHealth = 500;
     public int MaxHealth => maxHealth;
+
+    // Référence au GameOverPanel
+    public GameObject gameOverPanel;
+
+    // Cette fonction est appelée lorsque le GameObject est détruit
+    private void OnDestroy()
+    {
+        OnObjectDestroyed();
+    }
+
+    // Cette fonction contient la logique à exécuter lors de la destruction
+    private void OnObjectDestroyed()
+    {
+        // Affiche le GameOverPanel
+        if (gameOverPanel != null)
+        {
+            gameOverPanel.SetActive(true);
+        }
+        // Gèle la scène
+        Time.timeScale = 0;
+    }
 }
