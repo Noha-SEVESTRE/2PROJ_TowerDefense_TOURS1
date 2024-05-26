b2e8b1a03774d5a8b2a6cfd14c2e0f6f4a2c9d2d
Noha SEVESTRE <noha.sevestre@supinfo.com>
2024-05-26 17:18:05 +0200

    Bouton Tourelles + prefabs
    Panel tourelle terminé
    Prefabs de toutes les tourelles
    Modification du script TurretBuildManager et TurretShop

--- Assets/Scripts/TurretShop.cs
+++ Assets/Scripts/TurretShop.cs
@@ -13,4 +13,19 @@ public class TurretShop : MonoBehaviour
     {
         turretBuildManager.SetTurretToBuild(turretBuildManager.smallTurretPrefab);
     }
+
+    public void PurchaseMediumTurret()
+    {
+        turretBuildManager.SetTurretToBuild(turretBuildManager.MediumTurretPrefab);
+    }
+
+    public void PurchaseBigTurret()
+    {
+        turretBuildManager.SetTurretToBuild(turretBuildManager.BigTurretPrefab);
+    }
+
+    public void PurchaseGigaTurret()
+    {
+        turretBuildManager.SetTurretToBuild(turretBuildManager.GigaTurretPrefab);
+    }
 }
